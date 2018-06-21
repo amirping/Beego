92d51a030a9b97bf32958b6b4edeb99c4571e774
Med Amir Saadallah <amirs-m-s@live.com>
2018-06-21 17:49:53 +0100

    firebase last fix (w/ storage)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,29 @@ import { ErrorHandler, NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { StatusBar } from "@ionic-native/status-bar";
-import { Dialogs } from '@ionic-native/dialogs';
+import { Dialogs } from "@ionic-native/dialogs";
 
 import { MyApp } from "./app.component";
-import { FIREBASE_CREDENTIALS } from './firebase.credentials';
+import { FIREBASE_CREDENTIALS } from "./firebase.credentials";
 
 import { LandingPage } from "../pages/landing/landing";
 import { LoginPage } from "../pages/login/login";
 import { SignupStep1Page } from "../pages/signup_step1/signup_step1";
 import { SignupStep2Page } from "../pages/signup_step2/signup_step2";
 import { HomePage } from "../pages/home/home";
-import { ProfilPage } from '../pages/profil/profil';
-import { ListOfFollowsPage } from '../pages/list-of-follows/list-of-follows'
-import { ParametreDuComptePage } from '../pages/parametre-du-compte/parametre-du-compte';
+import { ProfilPage } from "../pages/profil/profil";
+import { ListOfFollowsPage } from "../pages/list-of-follows/list-of-follows";
+import { ParametreDuComptePage } from "../pages/parametre-du-compte/parametre-du-compte";
 import { ChilloutPage } from "../pages/chillout/chillout";
 
-import { Ionic2RatingModule } from 'ionic2-rating';
+import { Ionic2RatingModule } from "ionic2-rating";
 import { AngularFireModule } from "angularfire2";
 import { AngularFireAuthModule } from "angularfire2/auth";
 import { AngularFireDatabaseModule } from "angularfire2/database";
-import { AngularFireStorageModule } from "angularfire2/storage";
+// import { AngularFireStorageModule } from "angularfire2/storage";
 // import { AngularFirestoreModule } from "angularfire2/firestore";
 
-import { UserProvider } from '../providers/user/user';
-
-
+import { UserProvider } from "../providers/user/user";
 
 @NgModule({
   declarations: [
@@ -49,7 +47,7 @@ import { UserProvider } from '../providers/user/user';
     AngularFireModule.initializeApp(FIREBASE_CREDENTIALS),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
-    AngularFireStorageModule,
+    // AngularFireStorageModule,
     Ionic2RatingModule
   ],
   bootstrap: [IonicApp],
@@ -70,7 +68,7 @@ import { UserProvider } from '../providers/user/user';
     SplashScreen,
     Dialogs,
     UserProvider,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -1,10 +1,9 @@
-import { Injectable } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { User } from '../../models/user.interface';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { AngularFireStorage } from 'angularfire2/storage';
-import * as firebase from 'firebase/app';
-
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "angularfire2/auth";
+import { User } from "../../models/user.interface";
+import { AngularFireDatabase } from "angularfire2/database";
+// import { AngularFireStorage } from 'angularfire2/storage';
+import * as firebase from "firebase/app";
 
 /*
   Generated class for the UserProvider provider.
@@ -15,10 +14,12 @@ import * as firebase from 'firebase/app';
 @Injectable()
 export class UserProvider {
   user = {} as User;
-  constructor(public auth: AngularFireAuth,
-    public storage: AngularFireStorage,
-    public db: AngularFireDatabase) {
-    console.log('Hello UserProvider Provider');
+  constructor(
+    public auth: AngularFireAuth,
+    // public storage: AngularFireStorage,
+    public db: AngularFireDatabase
+  ) {
+    console.log("Hello UserProvider Provider");
   }
   isConnect() {
     return this.auth.authState;
@@ -29,17 +30,19 @@ export class UserProvider {
   setUser() {
     return new Promise((resolve, reject) => {
       const uid = this.auth.auth.currentUser.uid;
-      this.db.object(`uers/${uid}`).valueChanges().subscribe((user: any) => {
-        if (user) {
-          this.user = user;
-          this.user.uid = uid;
-        } else {
-          reject({ userIsNotSet: true });
-        }
-        resolve();
-      });
+      this.db
+        .object(`uers/${uid}`)
+        .valueChanges()
+        .subscribe((user: any) => {
+          if (user) {
+            this.user = user;
+            this.user.uid = uid;
+          } else {
+            reject({ userIsNotSet: true });
+          }
+          resolve();
+        });
     });
-
   }
   logOut() {
     this.auth.auth.signOut();
@@ -49,46 +52,51 @@ export class UserProvider {
   }
   signup(user: User, password: string, file: File) {
     return new Promise((resolve, reject) => {
-      this.auth.auth.createUserWithEmailAndPassword(user.email, password)
+      this.auth.auth
+        .createUserWithEmailAndPassword(user.email, password)
         .then(() => {
           const uid = this.auth.auth.currentUser.uid;
           if (file) {
-            this.storage.ref(`/users/${uid}/${this.GUID}`)
-              .put(file, { contentType: file.type })
-              .then(a => {
-                a.ref.getDownloadURL()
-                  .then(url => {
-                    user.photoUrl = url;
-                    this.db.list('uers').update(uid, user)
-                      .then(() => {
-                        resolve();
-                        this.auth.auth.currentUser.sendEmailVerification();
-                        this.logOut();
-                      });
-                  });
-              }, err => {
-                reject({ type: 0, err });
-              }).catch(err => {
-                reject({ type: 1, err });
-              });
+            // this.storage.ref(`/users/${uid}/${this.GUID}`)
+            //   .put(file, { contentType: file.type })
+            //   .then(a => {
+            //     a.ref.getDownloadURL()
+            //       .then(url => {
+            //         user.photoUrl = url;
+            //         this.db.list('uers').update(uid, user)
+            //           .then(() => {
+            //             resolve();
+            //             this.auth.auth.currentUser.sendEmailVerification();
+            //             this.logOut();
+            //           });
+            //       });
+            //   }, err => {
+            //     reject({ type: 0, err });
+            //   }).catch(err => {
+            //     reject({ type: 1, err });
+            //   });
           } else {
-            this.db.list('uers').update(uid, user)
+            this.db
+              .list("uers")
+              .update(uid, user)
               .then(() => {
                 resolve();
                 this.auth.auth.currentUser.sendEmailVerification();
                 this.logOut();
               });
           }
-        }).catch(err => {
+        })
+        .catch(err => {
           reject({ type: 2, err });
         });
     });
   }
   loginWithGoogle(nextStep, success) {
-    alert("here")
-    this.auth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    alert("here");
+    this.auth.auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
       .then(res => {
-        alert("and here")
+        alert("and here");
         const af = res.additionalUserInfo;
         if (af.isNewUser) {
           const user = {
@@ -96,33 +104,36 @@ export class UserProvider {
             gender: af.profile.gender,
             photoUrl: af.profile.picture,
             firstName: af.profile.given_name,
-            lastName: af.profile.family_name,
-          }
+            lastName: af.profile.family_name
+          };
           nextStep(user);
         } else {
           console.log("is not new");
-          this.setUser().then(() => {
-            console.log("is allready regsitred");
-            success();
-          }).catch(e => {
-            console.log("err", e);
-            if (e.userIsNotSet) {
-              const user = {
-                email: af.profile.email,
-                gender: af.profile.gender,
-                photoUrl: af.profile.picture,
-                firstName: af.profile.given_name,
-                lastName: af.profile.family_name,
+          this.setUser()
+            .then(() => {
+              console.log("is allready regsitred");
+              success();
+            })
+            .catch(e => {
+              console.log("err", e);
+              if (e.userIsNotSet) {
+                const user = {
+                  email: af.profile.email,
+                  gender: af.profile.gender,
+                  photoUrl: af.profile.picture,
+                  firstName: af.profile.given_name,
+                  lastName: af.profile.family_name
+                };
+                nextStep(user);
               }
-              nextStep(user);
-            }
-          });
+            });
         }
       })
-      .catch(e => console.log(e))
+      .catch(e => console.log(e));
   }
   loginWithFacebook(nextStep, success) {
-    this.auth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+    this.auth.auth
+      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
       .then(res => {
         console.log(res);
         const kf = res.additionalUserInfo;
@@ -133,47 +144,58 @@ export class UserProvider {
             gender: kf.profile.gender,
             photoUrl: kf.profile.picture.data.url,
             firstName: kf.profile.first_name,
-            lastName: kf.profile.last_name,
-          }
+            lastName: kf.profile.last_name
+          };
           nextStep(user);
         } else {
           console.log("is not new");
-          this.setUser().then(() => {
-            console.log("is allready regsitred");
-            success();
-          }).catch(e => {
-            console.log("err", e);
-            if (e.userIsNotSet) {
-              const user = {
-                email: kf.profile.email,
-                gender: kf.profile.gender,
-                photoUrl: kf.profile.picture.data.url,
-                firstName: kf.profile.first_name,
-                lastName: kf.profile.last_name,
+          this.setUser()
+            .then(() => {
+              console.log("is allready regsitred");
+              success();
+            })
+            .catch(e => {
+              console.log("err", e);
+              if (e.userIsNotSet) {
+                const user = {
+                  email: kf.profile.email,
+                  gender: kf.profile.gender,
+                  photoUrl: kf.profile.picture.data.url,
+                  firstName: kf.profile.first_name,
+                  lastName: kf.profile.last_name
+                };
+                nextStep(user);
               }
-              nextStep(user);
-            }
-          });
+            });
         }
       })
-      .catch(e => console.log(e))
+      .catch(e => console.log(e));
   }
   signupWithProvider(user) {
     console.log(user);
     console.log(this.auth.auth.currentUser.uid);
     return new Promise((resolve, reject) => {
-      this.db.list('uers').update(this.auth.auth.currentUser.uid, user)
+      this.db
+        .list("uers")
+        .update(this.auth.auth.currentUser.uid, user)
         .then(() => {
           resolve();
-        }).catch(e => {
-          reject(e);
         })
-    })
+        .catch(e => {
+          reject(e);
+        });
+    });
   }
   resetPassword(email: string) {
     return this.auth.auth.sendPasswordResetEmail(email);
   }
   get GUID(): string {
-    return new Date().getTime().toString(36) + "_" + Math.random().toString(36).substring(2, 10);
+    return (
+      new Date().getTime().toString(36) +
+      "_" +
+      Math.random()
+        .toString(36)
+        .substring(2, 10)
+    );
   }
 }
