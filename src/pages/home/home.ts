ec1fec708125220ac469d11c8aab70b5bc2e2875
Hatem ABBES <hatem.aabbes@gmail.com>
2018-06-21 15:28:29 +0100

    home rebase + fix angular fire version

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { UserProvider } from '../../providers/user/user';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase } from 'angularfire2/database';
+
 
 /**
  * Generated class for the HomePage page.
@@ -15,17 +19,67 @@ import { UserProvider } from '../../providers/user/user';
   templateUrl: 'home.html',
 })
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
+
+  evenementListRef$: Observable<any[]>;
+  espacesListRef$: Observable<any[]>;
+  promotionListRef$: Observable<any[]>;
+  suggestionListRef$: Observable<any[]>;
+  connaissanceListRef$: Observable<any[]>;
+  proximiteListRef$: Observable<any[]>;
+  invitationListRef$: Observable<any[]>;
 
   constructor(public navCtrl: NavController,
-    public userProvider: UserProvider,
-    public navParams: NavParams) {
+     public navParams: NavParams,
+     private database: AngularFireDatabase,
+    ) {
+      /* Liste des espaces */
+      this.espacesListRef$ = this.database.list('espace').valueChanges();/*map(changes => {
+        return changes.map( c => ({key : c.payload.key,...c.payload.val()}))
+      });*/
+
+      /* Liste des suggestions */
+      this.suggestionListRef$ = this.database.list('suggestion').valueChanges();/*map(changes => {
+        return changes.map( c => ({key : c.payload.key,...c.payload.val()}))
+      });*/
+
+      /* Liste des connaissances */
+      this.connaissanceListRef$ = this.database.list('connaissance').valueChanges();/*map(changes => {
+        return changes.map( c => ({key : c.payload.key,...c.payload.val()}))
+      });*/
+
+      /* Liste des proximités */
+      this.proximiteListRef$ = this.database.list('proximite').valueChanges();/*map(changes => {
+        return changes.map( c => ({key : c.payload.key,...c.payload.val()}))
+      });*/
+
+      /* Liste des proximités */
+      this.invitationListRef$ = this.database.list('invitation').valueChanges();/*map(changes => {
+        return changes.map( c => ({key : c.payload.key,...c.payload.val()}))
+      });*/
+
+        /* Liste des evenements */
+      this.evenementListRef$ = this.database.list('evenement').valueChanges()/*.map(changes => {
+        return changes.map( c => ({key : c.payload.key,...c.payload.val()}))
+      })*/;
+      
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    console.log('ionViewDidLoad AcceuilPage');
   }
-  logout(){
-    this.userProvider.logOut();
+  promotionClicked(){
+    /* Liste des promotions */
+    this.evenementListRef$ = this.database.list('promotion').valueChanges()/*.map(changes => {
+      return changes.map( c => ({key : c.payload.key,...c.payload.val()}))
+    })*/;
   }
+  evenementClicked(){
+    /* Liste des evenements */
+    this.evenementListRef$ = this.database.list('evenement').valueChanges()/*.map(changes => {
+      return changes.map( c => ({key : c.payload.key,...c.payload.val()}))
+    })*/;
+  }
+
 
 }
