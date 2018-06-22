7d770af65a3dab4bce002fa22de1098498ed13c5
nour <nour191923@gmail.com>
2018-06-22 23:21:10 +0200

    setting_page2

---
+++ src/pages/setting-profil2/setting-profil2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SettingProfil2Page } from './setting-profil2';
+
+@NgModule({
+  declarations: [
+    SettingProfil2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(SettingProfil2Page),
+  ],
+})
+export class SettingProfil2PageModule {}
