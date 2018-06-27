df57f63942ea06a8ccb60cd0bc532ba31de30cda
Med Amir Saadallah <amirs-m-s@live.com>
2018-06-27 18:03:04 +0100

    basic impl for findFriend (not well animated + dynamic + miss parts)

--- src/pages/find-friend/find-friend.ts
+++ src/pages/find-friend/find-friend.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
 
 /**
  * Generated class for the FindFriendPage page.
@@ -10,16 +10,64 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-find-friend',
-  templateUrl: 'find-friend.html',
+  selector: "page-find-friend",
+  templateUrl: "find-friend.html"
 })
 export class FindFriendPage {
-
+  friends: any = [];
+  activeFriend = 0;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.friends.push({
+      id: 1,
+      pic: "http://lorempixel.com/900/900/people",
+      name: "Lamine ben zekri",
+      location: "Le bardo",
+      age: "24 ans"
+    });
+    this.friends.push({
+      id: 2,
+      pic: "http://lorempixel.com/900/900/people",
+      name: "Ahmed ben zekri",
+      location: "Le bardo",
+      age: "24 ans",
+      stat: 0
+    });
+    this.friends.push({
+      id: 3,
+      pic: "http://lorempixel.com/900/900/people",
+      name: "Moha ben zekri",
+      location: "Le bardo",
+      age: "24 ans",
+      stat: 0
+    });
+    this.friends.push({
+      id: 4,
+      pic: "http://lorempixel.com/900/900/people",
+      name: "Aimed ben zekri",
+      location: "Le bardo",
+      age: "24 ans",
+      stat: 0
+    });
+    this.friends.push({
+      id: 5,
+      pic: "http://lorempixel.com/900/900/people",
+      name: "Hmed ben zekri",
+      location: "Le bardo",
+      age: "24 ans",
+      stat: 0
+    });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FindFriendPage');
+    console.log("ionViewDidLoad FindFriendPage");
   }
-
+  acceptFriend(id) {
+    this.friends[id].stat = 1;
+    this.activeFriend = id+1
+  }
+  rejectFriend(id) {
+    this.friends[id].stat = -1;
+    this.activeFriend = id+1
+  }
+  swiperHandler(Event) {}
 }
