6f5415861186006254bc56d29b40de7a50968ac0
nour <nour191923@gmail.com>
2018-06-27 16:47:29 +0200

    friend unfollow

--- src/pages/friend-profil/friend-profil.ts
+++ src/pages/friend-profil/friend-profil.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProfilPage } from '../profil/profil';
+
+//const vibrant = require('node-vibrant');
 
 /**
  * Generated class for the FriendProfilPage page.
@@ -16,7 +19,65 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class FriendProfilPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
   }
+  dosomething(img,imgbgcolor){
+    
+    var rgb = getAverageRGB(img);
+    imgbgcolor.style.backgroundColor = 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')';
+    function getAverageRGB(imgEl) {
+    
+      var blockSize = 5, // only visit every 5 pixels
+          defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+          canvas = document.createElement('canvas'),
+          context = canvas.getContext && canvas.getContext('2d'),
+          data, width, height,
+          i = -4,
+          length,
+          rgb = {r:0,g:0,b:0},
+          count = 0;
+          
+      if (!context) {
+          return defaultRGB;
+      }
+      
+      height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+      width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+      
+      context.drawImage(imgEl, 0, 0);
+      
+      try {
+          data = context.getImageData(0, 0, width, height);
+      } catch(e) {
+          /* security error, img on diff domain */alert('x');
+          return defaultRGB;
+      }
+      
+      length = data.data.length;
+      
+      while ( (i += blockSize * 4) < length ) {
+          ++count;
+          rgb.r += data.data[i];
+          rgb.g += data.data[i+1];
+          rgb.b += data.data[i+2];
+      }
+      
+      // ~~ used to floor values
+      rgb.r = ~~(rgb.r/count);
+      rgb.g = ~~(rgb.g/count);
+      rgb.b = ~~(rgb.b/count);
+      
+      return rgb;
+      
+  }
+
+  }
+  folowsPage(){
+    this.navCtrl.push(ProfilPage);
+  }
+  
+  
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FriendProfilPage');
