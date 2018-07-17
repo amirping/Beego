1a03878f2593eddc52a8c0030dd1ac569eb9969a
Med Amir Saadallah <amirs-m-s@live.com>
2018-07-17 17:35:43 +0100

    bug #63 working on

---
+++ src/providers/hammerConfig/HammerVertical.ts
@@ -0,0 +1,14 @@
+import {
+  HammerGestureConfig,
+  HAMMER_GESTURE_CONFIG
+} from "@angular/platform-browser";
+export class HammerVertical extends HammerGestureConfig {
+  overrides = <any>{
+    /*pan: { direction: Hammer.DIRECTION_All },
+            swipe: { direction: Hammer.DIRECTION_VERTICAL }, */
+    swipe: { direction: 31 },
+    pan: {
+      direction: 31
+    }
+  };
+}
