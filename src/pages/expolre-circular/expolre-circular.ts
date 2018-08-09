493cff3a745a935b19c2a389f444c03fe4756a69
Med Amir Saadallah <amirs-m-s@live.com>
2018-08-09 17:36:49 +0100

    finsh the arc + taking care on large screen + selecting event

--- src/pages/expolre-circular/expolre-circular.ts
+++ src/pages/expolre-circular/expolre-circular.ts
@@ -16,7 +16,39 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
 export class ExpolreCircularPage {
   currentView = 1;
   distance = 12;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  tastes: Array<any> = [];
+  selected: Array<any> = [];
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.tastes.push(
+      { id: "0", pic: "cocktail.png", name: "cocktail" },
+      { id: "1", pic: "beer.png", name: "alcahool" },
+      { id: "2", pic: "hearts-and-arrow.png", name: "romantic" },
+      { id: "3", pic: "coffee-cup.png", name: "coffee" },
+      { id: "4", pic: "beer.png", name: "bar" },
+      { id: "5", pic: "clock_1_.png", name: "promo" },
+      { id: "6", pic: "cocktail.png", name: "pink" },
+      { id: "7", pic: "beer.png", name: "ac/dc" },
+      { id: "8", pic: "hearts-and-arrow.png", name: "lamb" },
+      { id: "9", pic: "beer.png", name: "nirvana" },
+      { id: "10", pic: "clock_1_.png", name: "farzit" },
+      { id: "11", pic: "cocktail.png", name: "gamra" },
+      { id: "12", pic: "beer.png", name: "lousif" },
+      { id: "13", pic: "hearts-and-arrow.png", name: "hazard" },
+      { id: "14", pic: "coffee-cup.png", name: "hattab" },
+      { id: "15", pic: "cocktail.png", name: "kahla" }
+    );
+  }
+  selectTaste(id) {
+    this.selected.push(this.tastes[id]);
+    this.tastes.splice(id, 0);
+    console.log(this.tastes);
+  }
+  removeTaste(taste) {
+    this.tastes.push(taste);
+    this.selected.splice(taste.id, 0);
+  }
+  swipeLeft(event) {}
+  swipeRight(event) {}
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad ExpolreCircularPage");
