9b0596b7088b8ce3860e9e66f35dc88a3b00ce1e
Med Amir Saadallah <amirs-m-s@live.com>
2018-08-10 16:10:09 +0100

    finish explore (v1)

--- src/pages/expolre-circular/expolre-circular.ts
+++ src/pages/expolre-circular/expolre-circular.ts
@@ -1,5 +1,10 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ViewController
+} from "ionic-angular";
 
 /**
  * Generated class for the ExpolreCircularPage page.
@@ -18,7 +23,12 @@ export class ExpolreCircularPage {
   distance = 12;
   tastes: Array<any> = [];
   selected: Array<any> = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  regions: Array<any> = [];
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public _view: ViewController
+  ) {
     this.tastes.push(
       { id: "0", pic: "cocktail.png", name: "cocktail" },
       { id: "1", pic: "beer.png", name: "alcahool" },
@@ -37,20 +47,113 @@ export class ExpolreCircularPage {
       { id: "14", pic: "coffee-cup.png", name: "hattab" },
       { id: "15", pic: "cocktail.png", name: "kahla" }
     );
+    this.regions.push(
+      {
+        id: "0",
+        name: "tunis",
+        selected: false
+      },
+      {
+        id: "1",
+        name: "tunis",
+        selected: false
+      },
+      {
+        id: "2",
+        name: "Hell Towen",
+        selected: false
+      },
+      {
+        id: "3",
+        name: "Bizerta",
+        selected: false
+      },
+      {
+        id: "4",
+        name: "Sousse",
+        selected: false
+      },
+      {
+        id: "5",
+        name: "Sfax",
+        selected: false
+      },
+      {
+        id: "6",
+        name: "Jandouba",
+        selected: false
+      },
+      {
+        id: "7",
+        name: "Ben Arous",
+        selected: false
+      },
+      {
+        id: "8",
+        name: "Manouba",
+        selected: false
+      },
+      {
+        id: "9",
+        name: "Kef",
+        selected: false
+      },
+      {
+        id: "10",
+        name: "Beja",
+        selected: false
+      }
+    );
   }
-  selectTaste(id) {
-    this.selected.push(this.tastes[id]);
-    this.tastes.splice(id, 0);
-    console.log(this.tastes);
+  //safe
+  selectTaste(item) {
+    if (this.selected.length < 3) {
+      // the problem here s whene w reomve an element the index become != item.id
+      let index = this.tastes.findIndex(it => it.id == item.id);
+      if (index != -1) {
+        this.selected.push(this.tastes[index]);
+        this.tastes.splice(index, 1);
+      }
+      // console.log(this.tastes);
+      // console.log(this.selected);
+    }
+    // show some message here
+    else {
+      console.log("dude we need only 3 ");
+    }
   }
-  removeTaste(taste) {
-    this.tastes.push(taste);
-    this.selected.splice(taste.id, 0);
+  // safe
+  removeTaste(taste, index) {
+    console.log(this.selected);
+    if (taste && this.selected[index]) {
+      this.tastes.push(taste);
+      this.selected.splice(index, 1);
+    }
   }
-  swipeLeft(event) {}
-  swipeRight(event) {}
+  swipeLeft(event) {
+    // take the first five elemnt to the end of the array
+    for (let i = 0; i <= 5; i++) {
+      let item = this.tastes.shift();
+      this.tastes.push(item);
+    }
+  }
+  swipeRight(event) {
+    for (let i = 0; i <= 5; i++) {
+      let item = this.tastes.pop();
+      this.tastes.splice(0, 0, item);
+    }
+  }
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad ExpolreCircularPage");
+  }
+  selectedCount() {
+    let count = 0;
+    this.regions.forEach(element => {
+      if (element.selected) {
+        count++;
+      }
+    });
+    return count;
   }
 }
