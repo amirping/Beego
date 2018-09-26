8a2a92144cbd82456b7ca5280a0d6331d411d38d
Med Amir Saadallah <amirs-m-s@live.com>
2018-09-26 20:16:28 +0100

    stable v1 - integration clean

--- src/pages/space-event-and-promotion/space-event-and-promotion.ts
+++ src/pages/space-event-and-promotion/space-event-and-promotion.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
-
+import * as moment from "moment";
 /**
  * Generated class for the SpaceEventAndPromotionPage page.
  *
@@ -15,6 +15,46 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
 })
 export class SpaceEventAndPromotionPage {
   page = "event";
+  event = {
+    date: moment().toISOString(),
+    time: moment().toISOString(),
+    titre: "",
+    pic: [
+      "https://source.unsplash.com/900x900/?party,neight",
+      "https://source.unsplash.com/1600x900/?party,club"
+    ],
+    dateEnd: moment()
+      .add(1, "day")
+      .toISOString(),
+    timeEnd: moment().toISOString(),
+    place: "",
+    access: ""
+  };
+  promotion = {
+    date: moment().toISOString(),
+    time: moment().toISOString(),
+    titre: "",
+    pic: [
+      "https://source.unsplash.com/900x900/?food,resturant",
+      "https://source.unsplash.com/1600x900/?party,drink"
+    ],
+    dateEnd: moment()
+      .add(1, "day")
+      .toISOString(),
+    timeEnd: moment().toISOString(),
+    filtre: true,
+    filtreAge: {
+      lower: 15,
+      upper: 45
+    },
+    filtreSexe: { f: true, m: false },
+    filtreLoc: "",
+    filtreLocLst:[
+      {name:"zahrouni fllower city",val:'zfc'},
+      { name: "El nasser 2", val: 'nas2' },
+      { name: "Bardo", val: 'bardo' }
+    ]
+  };
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
